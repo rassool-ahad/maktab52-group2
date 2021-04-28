78a0f4dfe4695aba6ee1d33f66ba9cf19a7e5e8b
rassool-ahad <rasoolahad1997@gmail.com>
2021-04-28 19:03:25 +0430

    check last

--- xo/xo.py
+++ xo/xo.py
@@ -67,15 +67,15 @@ class _XOGame(_XOTable):
         elif player == 0 or player == 1:
             player = self.player1 if player == 0 else self.player2
         elif isinstance(player, _Player) is False:
-            raise self.InvalidPlayer(player1, "invalid player")
+            raise self.InvalidPlayer("player1", "invalid player")
         table = _XOTable()
         table.mark(cell_no, player.sign)
         print(table)
 
-        
     def winner(self) -> Optional[_Player]:
         if None in self.table.xo_map.values() or self.rounds < 5:
             raise self.UnFinishedGameError("The Game has not Finished yet!...")
         if len(set(self.scores.values())) != 1:
             return self.player1 if self.scores[self.player1] > self.scores[self.player2] else self.player2
 
+print("rezaaaaaaAaaaaa")
